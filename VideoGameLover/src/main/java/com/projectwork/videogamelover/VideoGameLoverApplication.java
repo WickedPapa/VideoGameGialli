c979a31ca7bdcddcc02044cde33e22a39be9cb76
WickedPapa <p.montano1993@gmail.com>
2023-01-25 15:43:36 +0100

    Creato Progetto

---
+++ VideoGameLover/src/main/java/com/projectwork/videogamelover/VideoGameLoverApplication.java
@@ -0,0 +1,13 @@
+package com.projectwork.videogamelover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameLoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGameLoverApplication.class, args);
+	}
+
+}
---
+++ VideoGameLover/src/test/java/com/projectwork/videogamelover/VideoGameLoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectwork.videogamelover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameLoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
