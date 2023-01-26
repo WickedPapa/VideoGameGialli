3333e9e28dc6bbb67fb2e638119adf92832508cb
WickedPapa <p.montano1993@gmail.com>
2023-01-26 18:17:10 +0100

    aggiunti diversi template

--- Frontend/src/index.ts
+++ Frontend/src/index.ts
@@ -1,2 +1,7 @@
 import nav from "./navbar/navbar.html"
+import registrazione from "./registrazione/registrazione.html"
+
 document.getElementById("header").innerHTML = nav;
+document.getElementById("signUp").onclick = () => {
+    document.getElementById("main").innerHTML = registrazione;
+}
