7d3c0a8bb40764e6265bbc0b166d448b62ec27cc
codingLigth <pasquale.schiava.1996@gmail.com>
2023-01-27 10:50:53 +0100

    Aggiunto footer e icona notifica

---
+++ Frontend/src/body/mainCarousel/mainCarousel.ts
@@ -0,0 +1,1 @@
+import product from './mainCarousel.html';
---
+++ Frontend/src/footer/footer.ts
@@ -0,0 +1,9 @@
+import div from './footer.html';
+
+export function createFot() {
+    document.getElementById("footer").innerHTML = div;
+}
+
+export function setFot() {
+    document.getElementById("footer").innerHTML = div;
+}
