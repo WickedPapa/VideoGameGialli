53320b7ee289618abe58886dab13b1d2bec05cf9
WickedPapa <p.montano1993@gmail.com>
2023-01-27 17:48:21 +0100

    tasti nav quasi funzionanti

--- Frontend/src/navbar/navbar.ts
+++ Frontend/src/navbar/navbar.ts
@@ -1,18 +1,32 @@
-import nav from "./navbar.html"
+import nav from "./navbar.html";
 import createModal from "../modal/modal";
-import createModalSignUp from "../registrazione/registrazione"
+import annunci from "../annunci/annunci.html";
+import home from "../home/home.html";
+import profile from "../profiloUtente/profile.html";
+import gestioneUtenti from "../gestioneUtenti/gestioneUtenti.html";
 
 
 export function createNav() {
     document.getElementById("header").innerHTML = nav;
     createModal();
-    //createModalSignUp();
-    //createModalSignIn();
-    //createModalSignOut();
+    document.getElementById("logo").onclick = () => {
+        document.getElementById("main").innerHTML = home;
+    };
+    document.getElementById("insertions").onclick = () => {
+        document.getElementById("main").innerHTML = annunci;
+    };
+    document.getElementById("management").onclick = () => {
+        document.getElementById("main").innerHTML = gestioneUtenti;
+    };
+    document.getElementById("management").onclick = () => {
+        document.getElementById("main").innerHTML = profile;
+    };
 }
 
 export function setNav() {
     document.getElementById("header").innerHTML = nav;
 }
+
+
 
 export default createNav;
