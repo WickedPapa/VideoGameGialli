a96bb4e2e72dfaa38239e8777e7b8bfbb6b4a272
Pieroaffondo94 <pierantoniotrovato1994@gmail.com>
2023-01-27 15:36:26 +0100

    cofa

--- Frontend/src/annunci/annunci.ts
+++ Frontend/src/annunci/annunci.ts
@@ -1,7 +1,35 @@
 import annunci from './annunci.html'
 
+var list:any = [];
+function createList(){
+    let insertions=document.getElementById("insertionContent")
+    insertions.innerHTML="";
+    let ul = document.createElement("ul");
+    for(let l of list){
+        let li = document.createElement("li");
+        li.innerHTML=l;
+        ul.append(li);
+    }
+}
+
+function getAll(){
+    /*prende tutti gli annunci da 
+    backend facendo una fetch poi mette tutti gli annunci nella list*/
+    createList();
+
+}
+function selectByGenre(genre : string){
+    /*solo a livello frontend prende la list, e seleziona solo quelli che hanno il genre 
+    corretto poi aggiorna la list*/
+
+}
+function selectByYear(year : number){
+    /*solo a livello frontend prende la list, e seleziona solo quelli che hanno l'year 
+    corretto poi aggiorna la list*/
+}
+
 export function showInsertions(){
     let main = document.getElementById("main");
-    main.innerHTML += annunci;
+    main.innerHTML = annunci;
 }
 export default showInsertions;
