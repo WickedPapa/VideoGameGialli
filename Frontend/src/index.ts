7dfc2835f927933d65a39e8d09244ca5d4d30bb5
WickedPapa <p.montano1993@gmail.com>
2023-01-27 12:40:20 +0100

    tempCommit

--- Frontend/src/index.ts
+++ Frontend/src/index.ts
@@ -1,13 +1,14 @@
 import createHome from "./home/home";
 import createNav from "./navbar/navbar"
 import createFot from "./footer/footer"
-
+import showAllUsers from "./gestioneUtenti/gestioneUtenti"
 //import * as registrazionets from "./registrazione/registrazione"
 
 
 createNav();
 createHome();
 createFot();
+showAllUsers();
 //document.getElementById("submitSignUp").onclick = registrazionets.tryToSignUp;
 
 
