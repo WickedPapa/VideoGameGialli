ba5ce4413a04c665a973e32ca30cad918977ba59
codingLigth <pasquale.schiava.1996@gmail.com>
2023-01-30 09:39:27 +0100

    home

--- Frontend/src/home/home.ts
+++ Frontend/src/home/home.ts
@@ -3,6 +3,17 @@ import home from './home.html'
 export function createHome(){
     let main = document.getElementById('main');
     main.innerHTML += home;
+    let head = document.getElementsByTagName('head')[0];
+    head.innerHTML += '<link rel="stylesheet" href="./home.css">';
 }
 
 export default createHome;
+
+/*
+async function gettitle(title: string):{
+    const response = await fetch()
+    const data = await response.json();
+    return data;
+
+}
+*/
