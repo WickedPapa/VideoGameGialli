de97f3a014ec88dbe07c0f4ec6e68b9d1e7cd2cd
Pieroaffondo94 <pierantoniotrovato1994@gmail.com>
2023-02-02 23:13:08 +0100

    filtri su pagina videogames funzionanti (ammazza che palle tocca fare una fecci annidata)

--- Frontend/src/videogiochi/videogiochi.ts
+++ Frontend/src/videogiochi/videogiochi.ts
@@ -1,9 +1,13 @@
 import { data } from "jquery";
+import genre from "../interfaces/genre";
 import videogame from "../interfaces/videogame";
 import createVideoGamePage from "./creaVideogiochi";
 import createGameVisualization from "./visualizzaVideogioco"
 
 let gameList: videogame[] = [];
+let gameFilteredList: videogame[] = []
+let genres: string[] = []
+let years: number[] = []
 let gameView: number = 6;
 let type: string;
 
@@ -22,28 +26,35 @@ export function createGamePage() {
                 document.getElementById("addVideogame").setAttribute("hidden", "true");
                 type = "GUEST"
             }
-            getAllGames(); 
-           
+            fetch("/videogames")
+                .then((response) => (response.json()))
+                .then((data) => {
+                    gameList = [];
+                    for (let game of data) {
+                        gameList.push(game);                      
+                    }
+                    createGamePagination();
+                    showGames(1);
+                    assignGameFilters();
+                })
         })
 }
 
 
 
 function getAllGames() {
-    
+
     fetch("/videogames")
         .then((response) => (response.json()))
         .then((data) => {
-            gameList=[];
+            gameList = [];
             for (let game of data) {
                 gameList.push(game);
             }
-             createGamePagination();
-             showGames(1);
         })
-   
-        
 }
+
+
 
 function showGames(i: number) {
     let content = document.getElementById("VideogameContent");
@@ -64,10 +75,10 @@ function showGames(i: number) {
             col.setAttribute("style", "background-color: rgba(0,0,0,0.2)");
         }
 
-        
-        
+
+
         let gameTitle = document.createElement("h3");
-        gameTitle.setAttribute("class","my-4")
+        gameTitle.setAttribute("class", "my-4")
         let image = document.createElement("img");
         image.src = gameList[start].cover.link;
         image.width = 200;
@@ -76,25 +87,25 @@ function showGames(i: number) {
         image.onclick = () => {
             createGameVisualization(game)
         }
-        gameTitle.innerHTML = "<b>"+gameList[start].name+"</b>";
-        
+        gameTitle.innerHTML = "<b>" + gameList[start].name + "</b>";
+
         col.append(gameTitle, image);
         let addButton = document.createElement("button");
-        let id =gameList[start].id
+        let id = gameList[start].id
         if (type == "USER") {
             addButton.innerHTML = "Aggiungi alla tua Lista!"
-            addButton.setAttribute("class","my-3")
+            addButton.setAttribute("class", "my-3")
             addButton.onclick = () => {
-                
-                fetch("/user/game/"+id)
-                .then((response)=>(response.json()))
-                .then((data)=>{
-                    if(data){
-                        alert("Videogioco aggiunto!");
-                    }else{
-                        alert("Possiedi già questo giuoco!");
-                    }
-                });
+
+                fetch("/user/game/" + id)
+                    .then((response) => (response.json()))
+                    .then((data) => {
+                        if (data) {
+                            alert("Videogioco aggiunto!");
+                        } else {
+                            alert("Possiedi già questo giuoco!");
+                        }
+                    });
             }
             col.append(addButton);
         }
@@ -133,6 +144,134 @@ function createGamePagination() {
         pages.appendChild(li);
     }
 }
+
+
+
+
+
+function setAllGenres() {
+    ;
+    genres = []
+    for (let i = 0; i < gameList.length; i++) {
+        for (let j = 0; j < gameList[i].genre.length; j++) {
+            if (!(genres.includes(gameList[i].genre[j].genre))) {
+                genres.push(gameList[i].genre[j].genre)
+                let ul = document.getElementById("gameGenres");
+                let li = document.createElement("li");
+                li.id = gameList[i].genre[j].genre;
+                let btn = document.createElement("button");
+                btn.setAttribute("class", "dropdown-item");
+                btn.innerHTML = gameList[i].genre[j].genre;
+                btn.onclick = () => {
+                    searchByGameGenre(li.id);
+                }
+                ul.append(li);
+                li.append(btn);
+            }
+        }
+    }
+}
+
+
+function setAllYears() {
+    years = []
+    for (let i = 0; i < gameList.length; i++) {
+        if (!(years.includes(gameList[i].year))) {
+            years.push(gameList[i].year)
+            let ul = document.getElementById("gameYears");
+            let li = document.createElement("li");
+            li.id = "" + gameList[i].year;
+            let btn = document.createElement("button");
+            btn.setAttribute("class", "dropdown-item");
+            btn.innerHTML = "" + gameList[i].year;
+            btn.onclick = () => {
+                searchByGameYear(Number(li.id))
+
+            }
+            ul.append(li);
+            li.append(btn);
+        }
+    }
+}
+
+
+function searchByAllGames() {             /*La funziona del bottone che cerca tutti gli annunci senza filtri*/
+    getAllGames();
+    createGamePagination();
+    showGames(1);
+}
+
+
+function searchByGameTitle(title: string) {
+    gameFilteredList = []
+    for (let element of gameList) {
+        let gameName = element.name
+        if ((gameName.toUpperCase().includes(title.toUpperCase()))) {
+            gameFilteredList.push(element)
+        }
+    }
+    gameList = [];
+    gameList = gameFilteredList
+    createGamePagination();
+    showGames(1);
+    getAllGames();
+}
+
+
+function searchByGameGenre(genre: String) {
+    gameFilteredList = []
+    for (let element of gameList) {
+        for (let i = 0; i < element.genre.length; i++) {
+            if (element.genre[i].genre == genre) {
+                gameFilteredList.push(element);
+                i = element.genre.length;
+            }
+        }
+    }
+    gameList = [];
+    gameList = gameFilteredList
+    createGamePagination();
+    showGames(1);
+    getAllGames();
+}
+
+function searchByGameYear(year: number) {
+    gameFilteredList = []
+    for (let element of gameList) {
+        if (element.year == year) {
+            gameFilteredList.push(element)
+        }
+    }
+    gameList = [];
+    gameList = gameFilteredList
+    createGamePagination();
+    showGames(1);
+    getAllGames();
+}
+
+
+
+
+function assignGameFilters() {             /*Assegna le funzioni ai pulsanti nelle dropDownList da provare*/
+    let allGames = document.getElementById("selectAllGames")
+    allGames.onclick = () => {
+        searchByAllGames();
+    }
+
+    setAllGenres();
+
+    setAllYears();
+
+    let searchTitle = document.getElementById("searchTitleGame") as HTMLInputElement;
+    searchTitle.onkeydown = (e) => {
+        if (e.key == 'Enter') {
+            searchByGameTitle(searchTitle.value)
+        }
+    }
+}
+
+
+
 
 
 
